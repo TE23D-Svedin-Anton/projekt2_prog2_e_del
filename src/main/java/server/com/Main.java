11088adacc3c821bdc979288af469737520ea23b
anton svedin <anton.svedin@elev.ga.ntig.se>
2026-04-28 13:04:57 +0200

    Har fixat mave så den kan hämta verktyg och just hämta dem verktyg ajg behöver i main.java

--- src/main/java/server/com/Main.java
+++ src/main/java/server/com/Main.java
@@ -1,7 +1,19 @@
 package server.com;
+//Importerar Gson objekt
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+//Importerar Type så att gson kan omvandla data
+import java.lang.reflect.Type;
+//Importerar unirest objekt
+import kong.unirest.Unirest;
+import kong.unirest.HttpResponse;
+import kong.unirest.UnirestException;
+//Gör så vi kan ha arraylist för att lagra objekt
+import java.util.ArrayList;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String baseUrl = "http://10.151.168.5:3102/"; //URL till server tjänsten
     }
 }
