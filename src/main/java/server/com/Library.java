998e415d51dfe327dcdd445697a26de20207a545
anton svedin <anton.svedin@elev.ga.ntig.se>
2026-05-14 14:52:34 +0200

    Fixat så den kan hämta en book och en magazin

--- src/main/java/server/com/Library.java
+++ src/main/java/server/com/Library.java
@@ -20,19 +20,19 @@ import java.util.List;
 public class Library {
 
     // URL till server tjänsten
-    private String baseUrl = "http://10.151.168.5:3102/";
+    private String baseUrl = "http://localhost:3000/";
 
     // Skapa Gson instans
     private Gson gson = new Gson();
-    //Skapa listor av böcker och magaziner
-    private ArrayList<Book> Bookshelf;
-    private ArrayList<Magazine> Magazineshelf;
+    // Skapa listor av böcker och magaziner
+    private ArrayList<Book> bookShelf;
+    private ArrayList<Magazine> magazineShelf;
 
     // Konstruktorn
     public Library() {
 
-        Bookshelf = new ArrayList<>();
-        Magazineshelf = new ArrayList<>();
+        bookShelf = new ArrayList<>();
+        magazineShelf = new ArrayList<>();
     }
 
     public void fetchBooks() {
@@ -43,24 +43,53 @@ public class Library {
         Type PublicationType = new TypeToken<ArrayList<Book>>() {
         }.getType();
 
-        Bookshelf = gson.fromJson(json_data, PublicationType);
+        bookShelf = gson.fromJson(json_data, PublicationType);
 
-        for (Book b : Bookshelf) {
+        for (Book b : bookShelf) {
             System.out.println(b);
         }
     }
 
-    public void fetchMagazine() {
-         HttpResponse<String> get_all_response = Unirest.get(baseUrl+"magazines").asString();
+    public void fetchBook() {
+        HttpResponse<String> get_one_response = Unirest.get(baseUrl + "books" + "/2").asString();
 
-         String json_data = get_all_response.getBody();
+        String json_data = get_one_response.getBody();
 
-         Type PublicationType = new TypeToken <ArrayList<Magazine>>(){}.getType();
+        Book book = gson.fromJson(json_data, Book.class);
 
-         Magazineshelf = gson.fromJson(json_data, PublicationType);
+        bookShelf.add(book);
 
-        for (Magazine m : Magazineshelf) {
-        System.out.println(m);
+        for (Book b : bookShelf) {
+            System.out.println(b);
+        }
     }
-}
+
+    public void fetchMagazines() {
+        HttpResponse<String> get_all_response = Unirest.get(baseUrl + "magazines").asString();
+
+        String json_data = get_all_response.getBody();
+
+        Type PublicationType = new TypeToken<ArrayList<Magazine>>() {
+        }.getType();
+
+        magazineShelf = gson.fromJson(json_data, PublicationType);
+
+        for (Magazine m : magazineShelf) {
+            System.out.println(m);
+        }
+    }
+
+    public void fetchMagazine() {
+        HttpResponse<String> get_one_response = Unirest.get(baseUrl + "magazines" + "/2").asString();
+
+        String json_data = get_one_response.getBody();
+
+        Magazine magazine = gson.fromJson(json_data, Magazine.class);
+
+        magazineShelf.add(magazine);
+
+        for (Magazine m : magazineShelf) {
+            System.out.println(m);
+        }
+    }
 }
